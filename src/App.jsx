74ce9a8ee6f12c6853297ad34c5bfefb6671fac9
stocaimaza <stocaimaza@hotmail.com>
2023-05-31 12:40:44 -0300

    clase 13

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import Cart from './components/Cart/Cart';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CarritoProvider } from './context/CarritoContext';
+
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <CarritoProvider>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<ItemListContainer />} />
+            <Route path='/categoria/:idCategoria' element={<ItemListContainer />} />
+            <Route path='/item/:idItem' element={<ItemDetailContainer />} />
+            <Route path='*' element={<h2> Sitio en Construcción </h2>} />
+            <Route path='/cart' element = {<Cart/>} />
+          </Routes>
+        </CarritoProvider>
+      </BrowserRouter>
+
+
+
+
+    </>
+  );
+}
+
+export default App;
